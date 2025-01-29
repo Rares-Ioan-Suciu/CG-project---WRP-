eaf8fb4ae1fc12b893d8571e1aaa510c4bfb72c1
Rares-Ioan-Suciu <165493479+Rares-Ioan-Suciu@users.noreply.github.com>
2025-01-29 16:01:56 +0200

    new  files - not code files

---
+++ DependInfo.cmake
@@ -0,0 +1,29 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/rares655/test_project/Astar.cpp" "CMakeFiles/anotherName.dir/Astar.cpp.o" "gcc" "CMakeFiles/anotherName.dir/Astar.cpp.o.d"
+  "/home/rares655/test_project/delaunay_trig.cpp" "CMakeFiles/anotherName.dir/delaunay_trig.cpp.o" "gcc" "CMakeFiles/anotherName.dir/delaunay_trig.cpp.o.d"
+  "/home/rares655/test_project/dijkstra.cpp" "CMakeFiles/anotherName.dir/dijkstra.cpp.o" "gcc" "CMakeFiles/anotherName.dir/dijkstra.cpp.o.d"
+  "/home/rares655/test_project/discretization.cpp" "CMakeFiles/anotherName.dir/discretization.cpp.o" "gcc" "CMakeFiles/anotherName.dir/discretization.cpp.o.d"
+  "/home/rares655/test_project/experimentPart.cpp" "CMakeFiles/anotherName.dir/experimentPart.cpp.o" "gcc" "CMakeFiles/anotherName.dir/experimentPart.cpp.o.d"
+  "/home/rares655/test_project/main.cpp" "CMakeFiles/anotherName.dir/main.cpp.o" "gcc" "CMakeFiles/anotherName.dir/main.cpp.o.d"
+  "/home/rares655/test_project/read_points.cpp" "CMakeFiles/anotherName.dir/read_points.cpp.o" "gcc" "CMakeFiles/anotherName.dir/read_points.cpp.o.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake_clean.cmake
@@ -0,0 +1,23 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/anotherName.dir/Astar.cpp.o"
+  "CMakeFiles/anotherName.dir/Astar.cpp.o.d"
+  "CMakeFiles/anotherName.dir/delaunay_trig.cpp.o"
+  "CMakeFiles/anotherName.dir/delaunay_trig.cpp.o.d"
+  "CMakeFiles/anotherName.dir/dijkstra.cpp.o"
+  "CMakeFiles/anotherName.dir/dijkstra.cpp.o.d"
+  "CMakeFiles/anotherName.dir/discretization.cpp.o"
+  "CMakeFiles/anotherName.dir/discretization.cpp.o.d"
+  "CMakeFiles/anotherName.dir/experimentPart.cpp.o"
+  "CMakeFiles/anotherName.dir/experimentPart.cpp.o.d"
+  "CMakeFiles/anotherName.dir/main.cpp.o"
+  "CMakeFiles/anotherName.dir/main.cpp.o.d"
+  "CMakeFiles/anotherName.dir/read_points.cpp.o"
+  "CMakeFiles/anotherName.dir/read_points.cpp.o.d"
+  "anotherName"
+  "anotherName.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/anotherName.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
